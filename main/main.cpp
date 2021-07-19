e70d3006c24b4b9470544b393195ee8c7cbf4a20
David Theese <dtheese@users.noreply.github.com>
2021-07-18 17:22:17 -0700

    Add information on using the alternate mode of operation of the program version in directory main

--- main/main.cpp
+++ main/main.cpp
@@ -18,8 +18,13 @@ int main()
    start = clock();
 
 #if 1
+   // The parameter in this function call is the hand size (5 - 52).
    evaluate_all_possible_hands(5);
 #else
+   // The parameters in this function call are:
+   //    * The target hand rank
+   //    * The number of times the target hand rank must be achieved
+   //    * The hand size (5 - 52)
    average_random_hands_until_target_hand_rank_hit(hand_rank_t::ROYAL_FLUSH, 300, 5);
 #endif
 
