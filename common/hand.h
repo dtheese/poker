587a7cca449e03ca8f36d1b3221e1ba7d9ef34c5
David Theese <dtheese@users.noreply.github.com>
2021-07-20 17:49:03 -0700

    Correct assignment of enumerated values in hand_rank_t

--- common/hand.h
+++ common/hand.h
@@ -12,16 +12,20 @@
 using namespace std;
 
 enum class hand_rank_t {
-                          HIGH_CARD,
-                          ONE_PAIR,
-                          TWO_PAIR,
-                          THREE_OF_A_KIND,
-                          STRAIGHT,
-                          FLUSH,
-                          FULL_HOUSE,
-                          FOUR_OF_A_KIND,
-                          STRAIGHT_FLUSH,
-                          ROYAL_FLUSH
+                          First = 0,
+                          HIGH_CARD = First,
+                          ONE_PAIR = 1,
+                          TWO_PAIR = 2,
+                          THREE_OF_A_KIND = 3,
+                          STRAIGHT = 4,
+                          FLUSH = 5,
+                          FULL_HOUSE = 6,
+                          FOUR_OF_A_KIND = 7,
+                          STRAIGHT_FLUSH = 8,
+                          ROYAL_FLUSH = 9,
+                          Last = ROYAL_FLUSH,
+                          End = Last + 1,
+                          None = End + 1
                        };
 
 void print_hand_rank(hand_rank_t hand_rank);
