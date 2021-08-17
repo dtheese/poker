74e30aaacca25a4869f4224881dbdc65c00ed966
David Theese <dtheese@users.noreply.github.com>
2021-08-16 19:37:34 -0700

    Make singleton_initializer.cpp common

--- common/singleton_initializer.cpp
+++ common/singleton_initializer.cpp
@@ -1,6 +1,9 @@
 #include "deck.h"
+
+#ifdef USE_TABLE
 #include "hand_rank_table.h"
 #include "primes.h"
+#endif
 
 #include "singleton_initializer.h"
 
@@ -9,7 +12,11 @@ singleton_initializer_s singleton_initializer_s::instance;
 singleton_initializer_s::singleton_initializer_s()
 {
    // Explicitly initialize this program's lazily-initialized singletons.
+#ifdef USE_TABLE
    deck_s::getInstance();
    hand_rank_table_s::getInstance();
    primes_s::getInstance();
+#else
+   deck_s::getInstance();
+#endif
 }
