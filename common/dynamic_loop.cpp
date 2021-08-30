084fe7bc0e96d0a28dc9675b1945e0f6fc1e77fc
David Theese <dtheese@users.noreply.github.com>
2021-08-30 10:48:31 -0700

    Make dynamic loop parameters const, remove one that is unneeded

--- common/dynamic_loop.cpp
+++ common/dynamic_loop.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 dynamic_loop_t::dynamic_loop_t(
-                                 unsigned int first_object_index_p,
-                                 unsigned int num_objects_total_p,
-                                 unsigned int num_objects_to_select_p,
+                                 const unsigned int first_object_index_p,
+                                 const unsigned int num_objects_total_p,
+                                 const unsigned int num_objects_to_select_p,
                                  void (*operation_to_perform_p)(const indexes_t &)
                               ): first_object_index{first_object_index_p},
                                  num_objects_total{num_objects_total_p},
@@ -17,19 +17,17 @@ dynamic_loop_t::dynamic_loop_t(
    dynamic_loop_body(
                        first_object_index,
                        first_object_index + num_objects_total - num_objects_to_select,
-                       0,
-                       num_objects_to_select
+                       0
                     );
 }
 
 void dynamic_loop_t::dynamic_loop_body(
-                                         unsigned int first_index,
-                                         unsigned int last_index,
-                                         unsigned int current_depth,
-                                         unsigned int desired_depth
+                                         const unsigned int first_index,
+                                         const unsigned int last_index,
+                                         const unsigned int current_depth
                                       )
 {
-   if (current_depth == desired_depth)
+   if (current_depth == num_objects_to_select)
    {
       operation_to_perform(indexes);
 
@@ -44,8 +42,7 @@ void dynamic_loop_t::dynamic_loop_body(
          dynamic_loop_body(
                              i + 1,
                              last_index + 1,
-                             current_depth + 1,
-                             desired_depth
+                             current_depth + 1
                           );
 
          indexes.pop_back();
--- common/dynamic_loop.h
+++ common/dynamic_loop.h
@@ -11,23 +11,22 @@ class dynamic_loop_t
 {
    public:
       dynamic_loop_t(
-                       unsigned int first_object_index_p,
-                       unsigned int num_objects_total_p,
-                       unsigned int num_objects_to_select_p,
+                       const unsigned int first_object_index_p,
+                       const unsigned int num_objects_total_p,
+                       const unsigned int num_objects_to_select_p,
                        void (*operation_to_perform_p)(const indexes_t &)
                     );
 
    private:
       void dynamic_loop_body(
-                               unsigned int first_index,
-                               unsigned int last_index,
-                               unsigned int current_depth,
-                               unsigned int desired_depth
+                               const unsigned int first_index,
+                               const unsigned int last_index,
+                               const unsigned int current_depth
                             );
 
-      unsigned int first_object_index;
-      unsigned int num_objects_total;
-      unsigned int num_objects_to_select;
+      const unsigned int first_object_index;
+      const unsigned int num_objects_total;
+      const unsigned int num_objects_to_select;
       void (*operation_to_perform)(const indexes_t &);
 
       indexes_t indexes;
