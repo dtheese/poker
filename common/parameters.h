29ec6fd286563b05ca8ccf53baedafb634b2a352
David Theese <dtheese@users.noreply.github.com>
2021-09-14 15:00:36 -0700

    Comment possible values of parameters

--- common/parameters.h
+++ common/parameters.h
@@ -1,7 +1,10 @@
 #ifndef PARAMETERS_INCLUDED
 #define PARAMETERS_INCLUDED
 
+// Allowed values: 5 - 52
 constexpr unsigned int NUM_CARDS{5};
+
+// Allowed values: 1 - 4
 constexpr unsigned int MAX_THREADS{4};
 
 #endif
