de0aeab4e816620f0ebca1dd5bb4a3c5ad1550d7
David Theese <dtheese@users.noreply.github.com>
2021-09-15 19:33:05 -0700

    Perform compile-time checks that parameter values are in allowed ranges

--- common/parameters.h
+++ common/parameters.h
@@ -3,8 +3,10 @@
 
 // Allowed values: 5 - 52
 constexpr unsigned int NUM_CARDS{5};
+static_assert(NUM_CARDS >= 5 && NUM_CARDS <= 52, "NUM_CARDS must be in the range 5 - 52, inclusive");
 
 // Allowed values: 1 - 4
 constexpr unsigned int MAX_THREADS{4};
+static_assert(MAX_THREADS >= 1 && MAX_THREADS <= 4, "MAX_THREADS must be in the range 1 - 4, inclusive");
 
 #endif
