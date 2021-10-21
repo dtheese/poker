dbff16672b3c809bb0574905c5b9bc5384d8292e
David Theese <dtheese@users.noreply.github.com>
2021-10-21 16:13:45 -0700

    Move combination_encoder and combinations_table to common

--- common/parameters.h
+++ common/parameters.h
@@ -4,7 +4,7 @@
 #include "fundamental_types.h"
 
 // Allowed values: 5 - 52
-constexpr my_uint_t NUM_CARDS{5};
+constexpr my_uint_t NUM_CARDS{7};
 static_assert(NUM_CARDS >= 5 && NUM_CARDS <= 52, "NUM_CARDS must be in the range 5 - 52, inclusive");
 
 // Allowed values: true, false
