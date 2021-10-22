05dfd0a5594cf469125c842eca123886f394f47c
David Theese <dtheese@users.noreply.github.com>
2021-10-22 09:58:15 -0700

    Add hand_rank_lookup_table

--- main/hand_rank_lookup_table.h
+++ main/hand_rank_lookup_table.h
@@ -1,9 +1,22 @@
 #ifndef HAND_RANK_LOOKUP_TABLE_INCLUDED
 #define HAND_RANK_LOOKUP_TABLE_INCLUDED
 
+#include <array>
+
+using namespace std;
+
+#include "combination_encoder.h"
+#include "deck.h"
+#include "fundamental_types.h"
+#include "hand.h"
+#include "parameters.h"
+#include "utilities.h"
+
 class hand_rank_lookup_table_s
 {
    public:
+      using hand_rank_lookup_table_t = array<hand_rank_t, combinations(52, 5)>;
+
       hand_rank_lookup_table_s(const hand_rank_lookup_table_s &) = delete;
       void operator=(const hand_rank_lookup_table_s &) = delete;
 
@@ -13,10 +26,13 @@ class hand_rank_lookup_table_s
       hand_rank_lookup_table_s *operator&() = delete;
 
       static const hand_rank_lookup_table_s &getInstance();
+      const hand_rank_lookup_table_t &getLookupTable() const;
 
    private:
       hand_rank_lookup_table_s();
       ~hand_rank_lookup_table_s();
+
+      hand_rank_lookup_table_t hand_rank_lookup_table;
 };
 
 #include "hand_rank_lookup_table.inl"
--- main/hand_rank_lookup_table.inl
+++ main/hand_rank_lookup_table.inl
@@ -1,5 +1,21 @@
 inline hand_rank_lookup_table_s::hand_rank_lookup_table_s()
 {
+   const auto &deck{deck_s::getInstance().getDeck()};
+   array<my_uint_t, 5> indexes;
+
+   for (my_uint_t encoded_value{0}; encoded_value < combinations(52, 5); ++encoded_value)
+   {
+      combination_encoder_t<decltype(indexes), 52, 5>::decode(encoded_value, indexes);
+
+      array<card_t, 5> cards;
+      my_uint_t j{0};
+
+      for (auto i : indexes)
+         cards[j++] = deck[i];
+
+      hand_t hand{cards.data()};
+      hand_rank_lookup_table[encoded_value] = hand.hand_rank();
+   }
 }
 
 inline hand_rank_lookup_table_s::~hand_rank_lookup_table_s()
@@ -11,4 +27,11 @@ inline const hand_rank_lookup_table_s &hand_rank_lookup_table_s::getInstance()
    static hand_rank_lookup_table_s instance;
 
    return instance;
+}
+
+inline
+const hand_rank_lookup_table_s::hand_rank_lookup_table_t
+&hand_rank_lookup_table_s::getLookupTable() const
+{
+   return hand_rank_lookup_table;
 }
