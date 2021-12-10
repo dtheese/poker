cec11418e50fe2db98e4d4ebc1b473a5d214bf48
David Theese <dtheese@users.noreply.github.com>
2021-12-09 19:25:13 -0700

    Add inline to function declarations

--- common/utilities.h
+++ common/utilities.h
@@ -7,14 +7,17 @@ using namespace std;
 
 template<typename T>
 constexpr
+inline
 T &operator ++(T &e);
 
 template<typename T>
 constexpr
+inline
 T combinations(const T n, const T r);
 
 template<typename T>
 constexpr
+inline
 T power(T n, T r);
 
 #include "utilities.inl"
